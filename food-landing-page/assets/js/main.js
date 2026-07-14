466d1b2743d4f32ff366eada1f744b9a7a956e55
mnzomaddar461 <mrnaimzomaddar461@gmail.com>
2026-07-15 03:29:28 +0600

    Start learning  Basic JS

---
+++ food-landing-page/assets/js/main.js
@@ -0,0 +1,30 @@
+const age = 22;
+console.log(age);
+
+let temparature = 54;
+temparature = 58;
+console.log(typeof temparature);
+
+// Add 
+const applePrice = 50; // camel case
+const OrangePrice = 70; // pascal case
+const Total_Price = applePrice + OrangePrice; // senak case
+console.log(Total_Price);
+
+// Sub
+const CustormerPay = 500;
+const ShopCeperReturn = 70;
+const TotalReturn = CustormerPay - ShopCeperReturn;
+console.log(TotalReturn);
+
+// convert to string to number;
+const numberString = '20';
+const number = parseInt(numberString);
+console.log(numberString);
+
+// Mult *
+const TopTenMartTshart = 399;
+const purceseCustomer = 5;
+const SubTotal = TopTenMartTshart * purceseCustomer;
+console.log(SubTotal);
+
